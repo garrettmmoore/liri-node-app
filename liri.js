3da6221373bae19324531b9104d01aa56f7c2b0f
Garrett Moore <garrettmarkmoore@gmail.com>
2017-08-22 11:21:43 -0700

    Cleaned up do what it says

--- liri.js
+++ liri.js
@@ -116,23 +116,37 @@ switch(commands)
                 console.log('\n');
             }
         });
+        break;
+
+    case "do-what-it-says":
+
+        fs.readFile("random.txt", "utf8", function(error, data) {
+    
+      // If the code experiences any errors it will log the error to the console.
+      if (error) {
+        return console.log(error);
+      }
+    
+      // We will then print the contents of data
+      console.log(data);
+    
+      // Then split it by commas (to make it more readable)
+      var dataArr = data.split(",");
+    
+      // We will then re-display the content as an array for later use.
+      console.log(dataArr);
+
+      spotify.search({ type: 'track', query: dataArr[1] }, function(err, data) 
+      {
+          if (err) 
+          {
+              return console.log('Error occurred: ' + err);
+          }
+          console.log(data.tracks.items[1].name);
+          console.log(data.tracks.items[1].album.name);
+          console.log("Artists Name: " + data.tracks.items[1].artists[0].name); 
+      });  
+    
+    });
 }
 
-
-// fs.readFile("movies.txt", "utf8", function(error, data) {
-    
-//       // If the code experiences any errors it will log the error to the console.
-//       if (error) {
-//         return console.log(error);
-//       }
-    
-//       // We will then print the contents of data
-//       console.log(data);
-    
-//       // Then split it by commas (to make it more readable)
-//       var dataArr = data.split(",");
-    
-//       // We will then re-display the content as an array for later use.
-//       console.log(dataArr);
-    
-//     });
