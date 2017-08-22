bdc6d4ce0be4255e71a11b5be84bdfa92fa0cde0
Garrett Moore <garrettmarkmoore@gmail.com>
2017-08-21 21:18:49 -0700

    Updated .gitignore to exclude *-lock.json Updated liri.js to include necessary NPM packages Changed main in package.json

--- liri.js
+++ liri.js
@@ -1,3 +1,11 @@
+
+var request = require("request");
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
+var inquirer = require("inquirer");
+var fs = require("fs");
+
 var mediaKeys = require("./keys.js");
-var twitter = mediaKeys.twitterKeys;
-var spotify = mediaKeys.spotifyKeys;
+
+var userTwitter = mediaKeys.twitterKeys;
+var userSpotify = mediaKeys.spotifyKeys;
