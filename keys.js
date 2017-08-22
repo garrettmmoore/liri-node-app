5f966ea8a98d54b5b78d1aa22802fa56b4b66133
Garrett Moore <garrettmarkmoore@gmail.com>
2017-08-21 20:57:29 -0700

    Initialize application

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '5rQYexogqXqv1ve8hYR6SO2Mr',
+  consumer_secret: 'hAom1cekGlvaXgD1DmXXhGzOaDyWAdUM74nXHCbeNJl5MFcNrp',
+  access_token_key: '899018269633331200-AzsGioEoIsNTmpFvc9xYaB7Dtzqt5JV',
+  access_token_secret: 'D1sURvd3Yqvh2pTOORHTErKu6aYl6shPpVOa175P2MkhF'
+};
+
+exports.spotifyKeys = {
+    client_id:'5fcada95ee784c2f996ad60d23127970',
+    client_secret:'8e1ec6918e454b06928a52a48282cb08'
+};
---
+++ liri.js
@@ -0,0 +1,3 @@
+var mediaKeys = require("./keys.js");
+var twitter = mediaKeys.twitterKeys;
+var spotify = mediaKeys.spotifyKeys;
