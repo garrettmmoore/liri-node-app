6e8e982eb8cfa402dee549a5d13055355c0a7959
Garrett Moore <garrettmarkmoore@gmail.com>
2017-08-22 00:10:15 -0700

    "Added OMDB functionality"

--- liri.js
+++ liri.js
@@ -69,56 +69,56 @@ switch(commands)
             {
                 return console.log('Error occurred: ' + err);
             }
-            console.log(data.tracks.items[1].name); 
+            console.log(data.tracks.items[1].name);
+            console.log(data.tracks.items[1].album.name);
+            console.log("Artists Name:" + data.tracks.items[1].artists[0].name); 
         });        
         break;
 
     case "movie-this":
-        break;
-    case "do-what-it-says":
-        break;
-};
+        // Store all of the arguments in an array
+        var nodeArgs = process.argv;
+
+        // Create an empty variable for holding the movie name
+        var movieName = "";
+
+        // Loop through all the words in the node argument
+        // handle the inclusion of "+"s
+        for (var i = 2; i < nodeArgs.length; i++) 
+        {
+            if (i > 2 && i < nodeArgs.length) 
+            {
+                movieName = nodeArgs[i];
+            }
+            else 
+            {
+                movieName += nodeArgs[i];
+            }
+        }
+
+        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+
+        request(queryUrl, function(error, response, body) 
+        {
+            if (!error && response.statusCode === 200) 
+            {
+                console.log('\n');
+                console.log("Title: " + JSON.parse(body).Title);
+                console.log("Year: " + JSON.parse(body).Year);
+                console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+                console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Metascore);
+                console.log("Country Produced: " + JSON.parse(body).Country);
+                console.log("Language: " + JSON.parse(body).Language);
+                console.log('\n');
+                console.log("Plot: " + JSON.parse(body).Plot);
+                console.log('\n');
+                console.log("Actors: " + JSON.parse(body).Actors);
+                console.log('\n');
+            }
+        });
+}
 
 
-
-//Twitter
-/**
- * Stream statuses filtered by keyword
- * number of tweets per second depends on topic popularity
- **/
-// userTwitter.stream('statuses/filter', {track: 'twitter'},  function(stream) {
-//     stream.on('data', function(tweet) {
-//       console.log(tweet.text);
-//     });
-  
-//     stream.on('error', function(error) {
-//       console.log(error);
-//     });
-//   });
-
-// //Spotify
-// userSpotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-//     if (err) {
-//       return console.log('Error occurred: ' + err);
-//     }
-   
-//   console.log(data); 
-//   });
-
-// //OMDB
-// // Then run a request to the OMDB API with the movie specified
-// request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=40e9cece", function(error, response, body) {
-    
-//       // If the request is successful (i.e. if the response status code is 200)
-//       if (!error && response.statusCode === 200) {
-    
-//         // Parse the body of the site and recover just the imdbRating
-//         // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-//         console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
-//       }
-//     });
-
-// // fs
 // fs.readFile("movies.txt", "utf8", function(error, data) {
     
 //       // If the code experiences any errors it will log the error to the console.
@@ -136,4 +136,3 @@ switch(commands)
 //       console.log(dataArr);
     
 //     });
-    
