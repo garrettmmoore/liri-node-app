8f9c39682c35505a55dabe642b0367464087506b
Garrett Moore <garrettmarkmoore@gmail.com>
2017-10-17 17:42:33 -0700

    Update keys.js. Updated .gitignore

--- keys.js
+++ keys.js
@@ -1,11 +1,11 @@
 exports.twitterKeys = {
-  consumer_key: '5rQYexogqXqv1ve8hYR6SO2Mr',
-  consumer_secret: 'hAom1cekGlvaXgD1DmXXhGzOaDyWAdUM74nXHCbeNJl5MFcNrp',
-  access_token_key: '899018269633331200-AzsGioEoIsNTmpFvc9xYaB7Dtzqt5JV',
-  access_token_secret: 'D1sURvd3Yqvh2pTOORHTErKu6aYl6shPpVOa175P2MkhF'
+  consumer_key: '<insert key here>',
+  consumer_secret: '<insert key here>',
+  access_token_key: '<insert key here>',
+  access_token_secret: '<insert key here>'
 };
 
 exports.spotifyKeys = {
-  client_id: '5fcada95ee784c2f996ad60d23127970',
-  client_secret: '8e1ec6918e454b06928a52a48282cb08'
+  client_id: '<insert key here>',
+  client_secret: '<insert key here>'
 };
